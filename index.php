d90b1ed79e242e774c0ecf6c0dc0262786e157bc
trungbobeou <trunggawe1591@gmail.com>
2023-04-19 08:06:47 +0700

    Update

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="style.css">
+
+    <script src="js/angular.min.js"></script>
+    <script src="js/angular-route.min.js"></script>
+    <script src="js/angular-singlepage.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <button onclick="topFunction()" id="btn-scrollToTop">Top</button>
+
+   
+    
+    
+    <!-- navigation starts -->
+    <?php include 'pages/nav.html';?>
+    <!-- navigation ends -->
+
+    <!-- slider  section start -->
+    <?php include 'pages/slider.html';?>
+    <!-- slider  section end  -->
+
+    <!-- featured section starts  -->
+    <?php include 'pages/featured.html';?>
+    <!-- featured section ends  -->
+
+    <!-- about us section starts  -->
+    <?php include 'pages/about.html';?>
+    <!-- about us section ends  -->
+
+    <!-- gallery section starts  -->
+    <?php include 'pages/gallery.html';?>
+    <!-- gallery section ends  -->
+
+    <!-- menu section starts -->
+    <?php include 'pages/menu.html';?>
+    <!-- menu section ends -->
+
+    <!-- testimonial section starts -->
+    <?php include 'pages/testimonial.html';?>
+    <!-- testimonial section ends -->
+
+    <!-- staff section --- chef starts -->
+    <?php include 'pages/staff.html';?>
+    <!-- staff section --- chef ends -->
+
+    <!-- order a cake section starts -->
+    <?php include 'pages/order.html';?>
+    <!-- order a cake section ends -->
+
+    <!-- blog section starts -->
+    <?php include 'pages/blog.html';?>
+    <!-- blog section ends -->
+
+    <!-- footer section starts -->
+    <?php include 'pages/footer.html';?>
+    <!-- footer section ends -->
+
+    <!-- scripts -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/jquery.timepicker.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="main.js"></script>
+    <script src="js/scrollToTop.js"></script>
+</body>
+
+</html>
